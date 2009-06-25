a50a705249740fcf5bc31533439268b2312c8447
joel <agnel.joel@gmail.com>
2009-06-25 21:40:48 +0530

    fcp code to get list of children from parent inode - UNTESTED

--- fcp.c
+++ fcp.c
@@ -1,5 +1,5 @@
 /*
- * fcp - A fast copy mechanism for ext2/3
+ * fcp - A fast file copy utility for ext2/3
  *
  * Copyright (C) 2009 Joel Fernandes
  *
@@ -12,19 +12,44 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
+#define EXT2_GET_PARENT 100
+#define EXT2_SET_PARENT 101
+#define EXT2_GET_CHILDR 102
+#define END_CHILD_LIST 000
 #define USAGE "usage: %s [-n] [-v] filesystem source destination ...\n"
+
+struct child_inodes {
+  int *start;
+  int length;
+};
 
 int set_inode_parent(fd, i_no)
 {
-  return ioctl(fd, 100, i_no);
+  return ioctl(fd, EXT2_SET_PARENT, i_no);
 }
 
 int get_inode_parent(fd)
 {
   int ret = 0;
-  ioctl(fd, 101, &ret);
+  ioctl(fd, EXT2_GET_PARENT, &ret);
   return ret;
+}
+
+struct child_inodes get_inode_children(fd)
+{
+  struct child_inodes ci;
+  int * list, count = 0;
+  ioctl(fd, EXT2_GET_CHILDREN, list);
+  if(!list)
+    return NULL;
+  while(*list)
+    {
+      count++;
+      list++;
+    }
+  ci.start = list;
+  ci.length = count;
+  return ci;
 }
 
 int main(int argc, char **argv)
@@ -42,17 +67,32 @@ int main(int argc, char **argv)
       return 1;
     }
 
-  if (argv[1][0] == 's') {
-    if(argc < 4) {
+  switch(argv[1][0])
+    {
+    case 's':
+      if(argc < 4) {
+	perror("Usage: fcp g|c filename [value]\n");
+	return 1;
+      }
+      value = atoi(argv[3]);
+      return set_inode_parent(fd, value);
+    case 'g':
+      value = get_inode_parent(fd);
+      if(!value)
+	printf("no parent found\n");
+      else
+	printf("inode parent %d\n", value);    
+      break;
+    case 'l':
+      for(i=0; i<ci.length; i++)
+	{
+	  printf("child: %d\n", ci.list[i])
+	}      
+      break;
+    default:
       perror("Usage: fcp g|c filename [value]\n");
       return 1;
     }
-    value = atoi(argv[3]);
-    return set_inode_parent(fd, value);
-  } else {
-    value = get_inode_parent(fd);
-    printf("inode parent %d\n", value);    
-    return 0;
-  }
+
   return 0;
 }
