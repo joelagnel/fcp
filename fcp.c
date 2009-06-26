11220c8404b0c075b2bb92c5eb2954be0773fc05
joel <agnel.joel@gmail.com>
2009-06-26 18:37:06 +0530

    Added parent child ioctls

--- fcp.c
+++ fcp.c
@@ -1,66 +1,96 @@
 /*
  * fcp - A fast file copy utility for ext2/3
  *
- * Copyright (C) 2009 Joel Fernandes
+ * Copyright (C) 2009 Joel Fernandes.
  *
- * This file may be redistributed under the terms of the GNU General Public
- * License.
+ * Description: userpace utility to talk to the ext2 cow layer.
+ *
+ * For a long time I wished to have copy-on-write in ext2. Here's an implementation of
+ * copy-on-write in ext2, in < 500 lines of code (kernel patch + fcp).
+ * fcp heavily depends on the kernel patch which should compile on all new kernels.
+ *
+ * This is purely original and hard work, modifying and redestribution is allowed
+ * provided this header is kept intact. This file may be redistributed under the terms
+ * of the GNU General Public License.
  */
 
+#define _GNU_SOURCE 1
+ 
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#define EXT2_GET_PARENT 100
-#define EXT2_SET_PARENT 101
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define EXT2_SET_PARENT 100
+#define EXT2_GET_PARENT 101
 #define EXT2_GET_CHILDR 102
 #define END_CHILD_LIST 000
-#define USAGE "usage: %s [-n] [-v] filesystem source destination ...\n"
 
-struct child_inodes {
-  int *start;
-  int length;
-};
-
-int set_inode_parent(fd, i_no)
+int set_inode_parent(int fd, int i_no)
 {
   return ioctl(fd, EXT2_SET_PARENT, i_no);
 }
 
-int get_inode_parent(fd)
+int get_inode_parent(int fd)
 {
   int ret = 0;
   ioctl(fd, EXT2_GET_PARENT, &ret);
   return ret;
 }
 
-struct child_inodes get_inode_children(fd)
+int get_inode_children(int fd, int * list)
 {
-  struct child_inodes ci;
-  int * list, count = 0;
-  ioctl(fd, EXT2_GET_CHILDREN, list);
-  if(!list)
-    return NULL;
+  int count = 0;
+  ioctl(fd, EXT2_GET_CHILDR, list);
   while(*list)
     {
       count++;
       list++;
     }
-  ci.start = list;
-  ci.length = count;
-  return ci;
+  return count;
+}
+
+int create_child(int source_fd, char *dest)
+{
+  int fd, ret, source_inode;
+  long source_size = 0;
+  struct stat buf;
+  fd  = open(dest, O_WRONLY|O_CREAT|O_EXCL|O_LARGEFILE);
+  if(fd < 0)
+    {
+      printf("couldn't open destination file %s ", dest);
+      return 1;
+    }
+  ret = fstat(source_fd, &buf);
+  if(ret < 0)
+    {
+      printf("Stat failed on source fd\n");
+      return ret;
+    }
+  source_size = (long)buf.st_size;
+  source_inode = (int)buf.st_ino;
+  lseek(fd, source_size + 9000, SEEK_SET); /* make sure we're atleast one block away */
+  write(fd, "\0", 1);
+  set_inode_parent(fd, source_inode);
+  close(fd);
+  return 0;
 }
 
 int main(int argc, char **argv)
 {
-  int fd, ret, value;
+  int fd, source_fd, ret, value;
+  int *list, count, i, mv = 1;
+  char *source, *dest, *name, *suffix = ".root";
 
   if(argc < 3) {
     perror("Usage: fcp g|c filename [value]\n");
     return 1;
   }
-  fd = open(argv[2], O_RDWR);
+
+  fd = open(argv[2], O_RDONLY);
   if(fd < 0)
     {
       perror("couldn't open file");
@@ -69,6 +99,48 @@ int main(int argc, char **argv)
 
   switch(argv[1][0])
     {
+    case 'c':
+      mv = 0;
+    case 'r': /* fast copy */
+      if(argc < 4) {
+	perror("Usage: fcp g|c|r|l|n source-filename [destination-filename|value]\n");
+      }
+      else {
+	if(mv)
+	  {
+	    source = argv[2];
+	    close(fd);
+	    name = (char *)malloc(strlen(source) + strlen(suffix) + 1);
+	    strcpy(name, source);
+	    strcat(name, suffix);
+	    rename(source, name); /* the old inode is supposed to disappear, rename for now */
+	    fd = open(name, O_RDONLY);
+	    ret = create_child(fd, source);
+	    if(ret)
+	      {
+		printf("Couldn't create child for source %s\n", source);
+		return 1;
+	      }
+	    else
+	      printf("source child created\n");
+	  }
+        for(i = 3; i < argc; i++)
+	  {
+	    dest = argv[i];
+	    ret = create_child(fd, dest);
+	    printf("ret = %d\n", ret);
+	    if(ret)
+	      {
+		printf("ret=%d, Couldn't create child for destination %s.\n", ret, dest);
+		return 1;
+	      }
+	    else
+	      printf("destination child created: %s\n", dest);
+	  }
+	printf("All done.\n");
+	return 0;
+      }
+
     case 's':
       if(argc < 4) {
 	perror("Usage: fcp g|c filename [value]\n");
@@ -76,6 +148,7 @@ int main(int argc, char **argv)
       }
       value = atoi(argv[3]);
       return set_inode_parent(fd, value);
+
     case 'g':
       value = get_inode_parent(fd);
       if(!value)
@@ -83,11 +156,21 @@ int main(int argc, char **argv)
       else
 	printf("inode parent %d\n", value);    
       break;
+
     case 'l':
-      for(i=0; i<ci.length; i++)
+      list = (int *)malloc(sizeof(int) * 200);
+      count = get_inode_children(fd, list);
+      if(!count)
 	{
-	  printf("child: %d\n", ci.list[i])
-	}      
+	  printf("No children found.\n");
+	}
+      else
+	{
+	  for(i=0; i<count; i++)
+	    {
+	      printf("child: %d\n", list[i]);
+	    }      
+	}
       break;
     default:
       perror("Usage: fcp g|c filename [value]\n");
